73acb2e40c1ef603edefeb91d936bc28cc57d597
Ryan Fitzgerald <ryfitz@ymail.com>
2024-05-28 14:29:16 -0600

    seed.js fixed

--- scripts/seed.js
+++ scripts/seed.js
@@ -15,7 +15,8 @@ const ether = tokens
 const shares = ether
 
 async function main() {
-  // Fdtch Accounts
+
+  // Fetch accounts
   console.log(`Fetching accounts & network \n`)
   const accounts = await ethers.getSigners()
   const deployer = accounts[0]
@@ -25,70 +26,72 @@ async function main() {
   const investor4 = accounts[4]
 
   // Fetch Network
-  const {chainId} = await ethers.provider.getNetwork()
+  const { chainId } = await ethers.provider.getNetwork()
 
-  console.log(`Fetching token and transfering to accounts ... \n`)
+  console.log(`Fetching token and transferring to accounts...\n`)
 
   // Fetch Dapp Token
   const dapp = await ethers.getContractAt('Token', config[chainId].dapp.address)
-  console.log(`Dapp Token fetched: ${dapp.address} \n`)
+  console.log(`Dapp Token fetched: ${dapp.address}\n`)
 
   // Fetch USD Token
   const usd = await ethers.getContractAt('Token', config[chainId].usd.address)
-  console.log(`USD Token fetched: ${usd.address} \n`)
+  console.log(`USD Token fetched: ${usd.address}\n`)
 
-  //////////////////////////////////////////////////////////////
+
+  /////////////////////////////////////////////////////////////
   // Distribute Tokens to Investors
   //
 
   let transaction
 
-  // Send dapp tokens to investor1
+  // Send dapp tokens to investor 1
   transaction = await dapp.connect(deployer).transfer(investor1.address, tokens(10))
   await transaction.wait()
 
-  // Send dapp tokens to investor2
-  transaction = await dapp.connect(deployer).transfer(investor2.address, tokens(10))
+  // Send usd tokens to investor 2
+  transaction = await usd.connect(deployer).transfer(investor2.address, tokens(10))
   await transaction.wait()
 
-  // Send dapp tokens to investor3
+  // Send dapp tokens to investor 3
   transaction = await dapp.connect(deployer).transfer(investor3.address, tokens(10))
   await transaction.wait()
 
-  // Send dapp tokens to investor4
-  transaction = await dapp.connect(deployer).transfer(investor4.address, tokens(10))
+  // Send usd tokens to investor 4
+  transaction = await usd.connect(deployer).transfer(investor4.address, tokens(10))
   await transaction.wait()
 
-  //////////////////////////////////////////////////////////////
-  // Ading Liquidity
+
+  /////////////////////////////////////////////////////////////
+  // Adding Liquidity
   //
 
-  let ammount = tokens(100)
+  let amount = tokens(100)
 
-  console.log(`Fetching AMM... \n`)
+  console.log(`Fetching AMM...\n`)
 
   // Fetch AMM
   const amm = await ethers.getContractAt('AMM', config[chainId].amm.address)
-  console.log(`AMM Token fetched: ${amm.address} \n`)
+  console.log(`AMM fetched: ${amm.address}\n`)
 
-  transactoin = await dapp.connect(deployer).approve(amm.address, ammount)
+  transaction = await dapp.connect(deployer).approve(amm.address, amount)
   await transaction.wait()
 
-  transactoin = await usd.connect(deployer).approve(amm.address, ammount)
+  transaction = await usd.connect(deployer).approve(amm.address, amount)
   await transaction.wait()
 
   // Deployer adds liquidity
   console.log(`Adding liquidity...\n`)
-  transaction = await amm.connect(deployer).addLiquidity(ammount, ammount)
+  transaction = await amm.connect(deployer).addLiquidity(amount, amount)
   await transaction.wait()
 
-  //////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
   // Investor 1 Swaps: Dapp --> USD
   //
-  
+
   console.log(`Investor 1 Swaps...\n`)
 
-  // Investor appproves all tokens
+  // Investor approves all tokens
   transaction = await dapp.connect(investor1).approve(amm.address, tokens(10))
   await transaction.wait()
 
@@ -96,27 +99,27 @@ async function main() {
   transaction = await amm.connect(investor1).swapToken1(tokens(1))
   await transaction.wait()
 
-  //////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
   // Investor 2 Swaps: USD --> Dapp
   //
-  
+
   console.log(`Investor 2 Swaps...\n`)
-  
-  // Investor appproves all tokens
+  // Investor approves all tokens tokens
   transaction = await usd.connect(investor2).approve(amm.address, tokens(10))
   await transaction.wait()
-  
+
   // Investor swaps 1 token
   transaction = await amm.connect(investor2).swapToken2(tokens(1))
   await transaction.wait()
-  
-  //////////////////////////////////////////////////////////////
+
+
+  /////////////////////////////////////////////////////////////
   // Investor 3 Swaps: Dapp --> USD
   //
-  
+
   console.log(`Investor 3 Swaps...\n`)
 
-  // Investor appproves all tokens
+  // Investor approves all tokens
   transaction = await dapp.connect(investor3).approve(amm.address, tokens(10))
   await transaction.wait()
 
@@ -124,21 +127,22 @@ async function main() {
   transaction = await amm.connect(investor3).swapToken1(tokens(10))
   await transaction.wait()
 
-  //////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
   // Investor 4 Swaps: USD --> Dapp
   //
-  
+
   console.log(`Investor 4 Swaps...\n`)
 
-  // Investor appproves all tokens
+  // Investor approves all tokens
   transaction = await usd.connect(investor4).approve(amm.address, tokens(10))
   await transaction.wait()
 
-  // Investor swaps 5 token
+  // Investor swaps all 10 tokens
   transaction = await amm.connect(investor4).swapToken2(tokens(5))
   await transaction.wait()
 
-  console.log(`Finished \n`)
+  console.log(`Finished.\n`)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
